d24a9aba58c9ecc4c96e883abb086ff69f8a98ea
Student <example@example.com>
2019-10-24 13:41:46 -0700

    add js functionality to hide images once submitted and resubmitted

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,32 @@ $(document).ready(function(){
     var age = $("input:radio[name=age]:checked").val();
     var gender = $("input:radio[name=gender]:checked").val();
     var hair = $("input:radio[name=hair]:checked").val();
-    console.log(gender);
+    $(".hide").hide();
+
+    if (age === '1' && gender === 'male' && hair === "light") {
+      $("#a").show();
+    }
+    if (age === '1' && gender === 'male' && hair === "dark") {
+      $("#b").show();
+    }
+    if (age === '1' && gender === 'female' && hair === "light") {
+      $("#c").show();
+    }
+    if (age === '1' && gender === 'female' && hair === "dark") {
+      $("#d").show();
+    }
+    if (age === '2' && gender === 'male' && hair === "light") {
+      $("#e").show();
+    }
+    if (age === '2' && gender === 'male' && hair === "dark") {
+      $("#f").show();
+    }
+    if (age === '2' && gender === 'female' && hair === "light") {
+      $("#g").show();
+    }
+    if (age === '2' && gender === 'female' && hair === "dark") {
+      $("#h").show();
+    }
 
     event.preventDefault();
   });
